70d8a0733d1c1d9769d8bf06815ff6d48819cb91
Shivam Singh <roxshivamsingh@gmail.com>
2026-08-26 15:48:25 +0530

    ADDED: Card;

--- shared/src/commonMain/kotlin/com/roxshivamsingh/longdistance/App.kt
+++ shared/src/commonMain/kotlin/com/roxshivamsingh/longdistance/App.kt
@@ -5,7 +5,12 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -13,30 +18,38 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
 @Preview
 fun App() {
     MaterialTheme {
+
         Column(
-            modifier = Modifier
-                .background(MaterialTheme.colorScheme.primaryContainer)
+            modifier = Modifier.background(MaterialTheme.colorScheme.primaryContainer)
 //                .safeContentPadding()
                 .fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
+            verticalArrangement = Arrangement.Center,
         ) {
 
-            Card() {
-                Box() {
-                    Text(
-                        text = getHumanReadable,
-                        fontSize = 30.sp,
-                        fontWeight = FontWeight.SemiBold
-                    )
+            ElevatedCard(
+                colors = CardDefaults.cardColors(
+                    containerColor = MaterialTheme.colorScheme.surfaceVariant,
+                ),
+                modifier = Modifier.size(width = 300.dp, height = 100.dp),
+                elevation = CardDefaults.cardElevation(defaultElevation = 6.dp)
 
-                }
+            ) {
+
+                Text(
+                    text = getHumanReadable,
+                    fontSize = 25.sp,
+                    fontWeight = FontWeight.SemiBold,
+                    modifier = Modifier.padding(20.dp),
+                )
+
             }
 
         }
